73c0749a137950a2efd57a1dac06fa76e9ceb61d
Geo <geomo3@gmail.com>
2015-02-19 11:47:08 -0600

    Fixed missing lines in speedysurvey...now it should connect to the analytics account.

--- meta/speedysurvey.js
+++ meta/speedysurvey.js
@@ -1,6 +1,10 @@
 //SpeedySurvey.js v0.1 - a simple survey that collects quick responses to single questions!
 //Copyright (c) George Moe 2015
 
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-58038459-2']);
+_gaq.push(['_trackPageview']);
+
 (function() {
 var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
